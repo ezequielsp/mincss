0b48e8dad2ac9655532367f9aacd89a104f964ea
Ezequiel Pereira <ezequielsp@users.noreply.github.com>
2015-11-22 20:37:02 -0200

    New version multi-files

--- mincss.sh
+++ mincss.sh
@@ -1,28 +1,45 @@
 #!/bin/bash
-# Call the closure compiler to minify css. 
-# Takes a filename mycss.x.y.css and outputs mycss.x.y.min.css, a minified version.
+#: Title       : mincss
+#: Date        : 2015-11-22
+#: Author      : "Ezequiel S. Pereira" <ezequielsp@hotmail.com.br>
+#: Version     : 1.0
+#: Description : Call the closure stylesheets to minify css. 
+#: Options     : None
 
-# The location of the closure compiler jar. You'll probably need to edit this.
+# A POSIX variable
+OPTIND=1         # Reset in case getopts has been used previously in the shell.
 CLOSURE_COMPILER=/usr/local/jar/closure-stylesheets-20111230.jar
 
-case "$1" in
-""|"-h"|"--help")
-    echo "Usage: mincss path/to/css/file.css"
-    exit 1;
-    ;;
-esac
+# Initialize our own variables:
+output_file=""
 
-cssDir=`dirname $1`
-cssFile=`basename $1`
+USAGE="Usage: mincss -o myfile file1.css file2.css file3.css ... fileN.css"
 
-# There's no way to get "from the beginning to the Nth to last field" when
-# specifying a range via cut, but we can get "from the Nth field to the end".
-# By reversing the string, cutting, and reversing back, we get the desired
-# effect. The following gets myfile.min.css from myfile.css
-mincssFile=`echo $cssFile | rev | cut -d"." -f2- | rev`.min.css
+if [ "$#" -eq 0 ]; then
+	echo "$USAGE"
+	exit 1
+fi
 
-cssFilePath=$cssDir/$cssFile
-mincssFilePath=$cssDir/$mincssFile
+while getopts "o:" opt; do
+    case "$opt" in
+      o) output_file=$OPTARG ;;
+    esac
+done
+shift $((OPTIND-1))
 
-java -jar $CLOSURE_COMPILER $cssFilePath > $mincssFilePath
-echo "$cssFilePath minified. Now located at $mincssFilePath"
+[ "$1" = "--" ] && shift
+for i in $@; 
+  do 
+    if [ ! -f $i ] 
+    	then  
+		echo "Input file $i does not exist"
+		exit 1;
+	fi
+done;
+
+mincssFilePath="$output_file.min.css"
+if [ "$#" -gt 0 ]
+	then
+	java -jar $CLOSURE_COMPILER $@ > $mincssFilePath
+	echo "$@  minified. Now located at $mincssFilePath"
+fi
